43c4342bf78f27b2ef8128e9c4c2e248e360a46d
Michael Zaleski <me@mikezaleski.com>
2020-07-08 14:15:08 -0400

    Updates

---
+++ precache-manifest.7ad22a8fe8d4c0f628d90d128a3ca99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028ddbb6c62ae157ba3adab5d4ff6730",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "7914f40699b12f5b2d76",
+    "url": "/youtube-clone/static/js/2.3c7dfc9a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.3c7dfc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875b3f0e21655ad9ca7b",
+    "url": "/youtube-clone/static/js/main.0329c6a2.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
